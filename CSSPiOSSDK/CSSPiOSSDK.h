3695cc08d4626a63125017e3c984db55dc760db8
dannis <dannis@dannisdeiMac.local>
2015-01-26 22:50:29 +0800

    Initial Commit

---
+++ CSSPiOSSDK/CSSPiOSSDK.h
@@ -0,0 +1,13 @@
+//
+//  CSSPiOSSDK.h
+//  CSSPiOSSDK
+//
+//  Created by dannis on 15/1/26.
+//  Copyright (c) 2015年 cssp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CSSPiOSSDK : NSObject
+
+@end
---
+++ CSSPiOSSDK/CSSPiOSSDK.m
@@ -0,0 +1,13 @@
+//
+//  CSSPiOSSDK.m
+//  CSSPiOSSDK
+//
+//  Created by dannis on 15/1/26.
+//  Copyright (c) 2015年 cssp. All rights reserved.
+//
+
+#import "CSSPiOSSDK.h"
+
+@implementation CSSPiOSSDK
+
+@end
